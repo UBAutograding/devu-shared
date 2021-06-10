ee02f07dfc5ef9c7b2c4c558eb9bcf69d3a9aa6b
Michael Drewitt <mikedrewitt@gmail.com>
2021-06-09 22:00:56 -0400

    Auth

---
+++ src/types/auth.types.ts
@@ -0,0 +1,26 @@
+export type SchoolAuth = {
+  email: string
+  schoolId: string
+}
+
+export type ApiAuth = {
+  token: string
+}
+
+export type DeserializedRefreshToken = {
+  id: number
+}
+
+export type DeserializedToken = {
+  iat?: number
+  exp?: number
+  email: string
+}
+
+export type Provider = {
+  name: string
+  route: string
+  description: string
+  method: 'get' | 'post'
+  body?: string[]
+}
