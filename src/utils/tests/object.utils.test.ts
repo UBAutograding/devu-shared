875b051cce68f265f2aed45fd75e123ebfe85ef2
Michael Drewitt <mikedrewitt@gmail.com>
2021-06-15 20:15:29 -0400

    Rename vars in test

--- src/utils/tests/object.utils.test.ts
+++ src/utils/tests/object.utils.test.ts
@@ -1,23 +1,23 @@
 import { renameKeys } from '../object.utils'
 
-const sampleMap = { oldKeyOne: 'newKeyOne', oldKeyTwo: 'newKeyTwo' }
+const attributeMap = { oldKeyOne: 'newKeyOne', oldKeyTwo: 'newKeyTwo' }
 describe('Object utils', () => {
   describe('renameKeys', () => {
     test('Changes keys based on map', () => {
       const oldKeyOne = 'someValue1'
       const oldKeyTwo = 'someValue2'
-      const oldMap: any = { oldKeyOne, oldKeyTwo }
+      const inputMap: any = { oldKeyOne, oldKeyTwo }
 
-      renameKeys(sampleMap, oldMap)
+      renameKeys(attributeMap, inputMap)
 
-      expect(oldMap.newKeyOne).toEqual(oldKeyOne)
-      expect(oldMap.newKeyTwo).toEqual(oldKeyTwo)
+      expect(inputMap.newKeyOne).toEqual(oldKeyOne)
+      expect(inputMap.newKeyTwo).toEqual(oldKeyTwo)
     })
 
     test('Keys missing in rename map remain untouched', () => {
       const unmatchedKeyValue = 'someValue3'
       const oldMap = { oldKeyOne: 'someValue', oldKeyTwo: 'someValue2', oldKeyThree: unmatchedKeyValue }
-      renameKeys(sampleMap, oldMap)
+      renameKeys(attributeMap, oldMap)
 
       expect(oldMap.oldKeyThree).toBeDefined()
       expect(oldMap.oldKeyThree).toEqual(unmatchedKeyValue)
@@ -25,7 +25,7 @@ describe('Object utils', () => {
 
     test('Original key names are deleted', () => {
       const oldMap = { oldKeyOne: 'someValue', oldKeyTwo: 'someValue2' }
-      renameKeys(sampleMap, oldMap)
+      renameKeys(attributeMap, oldMap)
 
       expect(oldMap.oldKeyOne).toBeUndefined()
       expect(oldMap.oldKeyTwo).toBeUndefined()
