b6b9d352b2f30af4a6acffdce0bc6459e1a3682d
Jesse Hartloff <hartloff@buffalo.edu>
2021-07-05 13:22:11 -0400

    Update levels to be a type instead of a list of literals

--- src/types/user-course.types.ts
+++ src/types/user-course.types.ts
@@ -3,8 +3,24 @@ export type UserCourse = {
   userId: number
   courseId: number
   dropped?: boolean
-  level?: "student" | "ta" | "instructor"
+  level?: UserCourseLevel
   lectureSection?: string
   createdAt?: string
   updatedAt?: string
 }
+
+// These roles are to be used by the front end to generate forms with labels for each level
+export type UserCourseRole = {
+  label: string
+  value: string
+}
+export const userCourseRoles: UserCourseRole[] = [
+  { label: 'Student', value: 'student' },
+  { label: 'TA', value: 'ta' },
+  { label: 'Instructor', value: 'instructor' },
+]
+
+// I was unable to find a way to remove the duplication here. I want to create a type based on the
+// userCourseRoles.value to avoid having each value in 2 places. If anyone knows how to do this, please share!
+const userCourseLevels = ['student', 'ta', 'instructor'] as const
+export type UserCourseLevel = typeof userCourseLevels[number]
