aefbc384773bee66c4ac9a507b875377c4fddba4
Jesse Hartloff <hartloff@buffalo.edu>
2021-07-19 09:39:14 -0400

    Make grading type option explicit instead of plain string

--- src/types/assignment.types.ts
+++ src/types/assignment.types.ts
@@ -14,3 +14,31 @@ export type Assignment = {
   createdAt?: string
   updatedAt?: string
 }
+
+
+export type UserCourse = {
+  id?: number
+  userId: number
+  courseId: number
+  dropped?: boolean
+  level?: GradingType
+  lectureSection?: string
+  createdAt?: string
+  updatedAt?: string
+}
+
+// These roles are to be used by the front end to generate forms with labels for each level
+export type gradingTypeDetail = {
+  label: string
+  value: string
+}
+export const gradingTypeDetails: gradingTypeDetail[] = [
+  { label: 'Autograded - Code', value: 'code' },
+  { label: 'Autograded - Not Code', value: 'non-code' },
+  { label: 'Manually Graded', value: 'manual' },
+]
+
+// I was unable to find a way to remove the duplication here. I want to create a type based on the
+// userCourseRoles.value to avoid having each value in 2 places. If anyone knows how to do this, please share!
+export const gradingTypes = ['code', 'non-code', 'manual'] as const
+export type GradingType = typeof gradingTypes[number]
