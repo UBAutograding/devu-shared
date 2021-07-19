5b3c840da29388c2f98675be523c4b25a3726b7b
Michael Drewitt <mikedrewitt@gmail.com>
2021-07-19 18:22:09 -0400

    lecture section is nullable not optional

--- src/types/userCourse.types.ts
+++ src/types/userCourse.types.ts
@@ -4,7 +4,7 @@ export type UserCourse = {
   courseId: number
   dropped: boolean
   level: UserCourseLevel
-  lectureSection?: string
+  lectureSection: string | null
   createdAt?: string
   updatedAt?: string
 }
