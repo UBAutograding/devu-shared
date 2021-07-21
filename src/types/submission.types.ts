6a10481e20dc372d9a7a3c31edc4cfe4d1ddb0c4
Michael Drewitt <mikedrewitt@gmail.com>
2021-07-21 18:18:57 -0400

    Fix submitted by

--- src/types/submission.types.ts
+++ src/types/submission.types.ts
@@ -9,7 +9,7 @@ export type Submission = {
 
   // submitterId can be different from userId if a submission was created on behalf of the student (regrade)
   // originalSubmissionId is only used on regrades
-  submittedBy: number | null
+  submittedBy?: number
   originalSubmissionId: number | null
 
   createdAt?: string
